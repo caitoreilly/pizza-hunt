b2c154a6d79c4b67aa5615a86ce537aa26808367
Caitlin O'Reilly <coreilly29@gmail.com>
2022-11-25 19:58:36 -0500

    set up front end comment functionality, create comment reply schema, update comment controller and routes, integrate reply functionality with front end

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // route for /api/comments/:pizzaId
@@ -9,7 +11,10 @@ const {
 router.route("/:pizzaId").post(addComment);
 
 // route for /api/comments/:pizzaId/:commentId
-// use removeComment() method as a DELETLE callback 
-router.route("/:pizzaId/:commentId").delete(removeComment);
+// use removeComment() method as a DELETLE callback
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+// DELETE route to handle removeReply
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
